152000c52204781f69f426ba9f18b669b9fabd4d
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-02 15:15:12 +0300

    test: test dev deploy branch

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(cors({ origin: "*" }));
 app.use("/api", mainRoutes);
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello Valera, this page for you!");
 });
 
 app.listen(port, () => {

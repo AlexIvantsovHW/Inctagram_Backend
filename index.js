ce90628a3600d0341485e0cdf1c45030298090e5
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-02 14:48:38 +0300

    fix vercel routes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({ origin: "*" }));
-app.use("/", mainRoutes);
+app.use("/api", mainRoutes);
 
 app.listen(port, () => {
   console.log(`Server is started on ${port} port.`);

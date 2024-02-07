c40a44421a45325b1e1368403c4e0327341ee728
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-07 11:58:42 +0300

    feat: add password and email validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,20 @@
 import { ObjectId } from "mongodb";
 import { User } from "./../../shared/db/models/user.module.js";
+import { Validate } from "./../routes/registration/services/validation.js";
 export const handleCatch = (res, error) => {
   res.status(500).json({ error });
 };
-
+export const validateForm = (res, message) => {
+  return res.status(500).send(message);
+};
 export const createUser = (req, res) => {
+  console.log(req.body);
+  let password = req.body.password;
+  let email = req.body.email;
+  const dataValidate = new Validate(password, email);
+  if (dataValidate.fullFormValidation() != true) {
+    return res.status(500).send(dataValidate.fullFormValidation());
+  }
   const user = new User(req.body);
   user
     .save()

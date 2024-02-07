d5a614e9a96f0b51cb8a6b1780226855c4869917
stasikrus <stas-boss@mail.ru>
2024-02-07 19:04:26 +0300

    Login feat: Added user authentication, implemented password hashing, and introduced session support

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 import mainRoutes from "./src/routes/mainRoutes.js";
 import mongoose from "mongoose";
 import { dbURL } from "./shared/configs/configs.js";
+import passport from 'passport';
+import session from 'express-session';
+import { initialPassport } from './shared/configs/passport-config.js';
 
 const app = express();
 const port = process.env.PORT | 3001;
@@ -21,6 +24,17 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+
+initialPassport(passport);
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors({ origin: "*" }));
 app.use("/api", mainRoutes);
